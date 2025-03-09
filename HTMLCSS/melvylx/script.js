a10f26e5d1467f2943b5d15e30708105b9c89616
sylvielsstfr <dagoret@lal.in2p3.fr>
2025-03-09 08:10:17 +0100

    update

--- HTMLCSS/melvylx/script.js
+++ HTMLCSS/melvylx/script.js
@@ -1,2 +1,37 @@
-
+// write a lot of stuff here
 console.log("Hello World");
+
+// write a function to get the current date
+function getCurrentDate() {
+  return new Date();
+}
+
+// write a function to get the current time
+function getCurrentTime() {
+  return new Date();
+}
+
+// write a function to get the current date and time
+function getCurrentDateTime() {
+  return new Date();
+}
+
+// write a function to get the current date and time in a specific format
+function getCurrentDateTimeInFormat(format) {
+  return new Date().toLocaleString(format);
+}
+
+// write a function to get the current date and time in a specific format
+function getCurrentDateTimeInFormat(format) {
+  return new Date().toLocaleString(format);
+}
+
+// write a function to get the current date and time in a specific format
+function getCurrentDateTimeInFormat(format) {
+  return new Date().toLocaleString(format);
+}
+
+// write a function to get the current date and time in a specific format
+function getCurrentDateTimeInFormat(format) {
+  return new Date().toLocaleString(format);
+}
